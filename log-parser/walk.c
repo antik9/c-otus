fbe335ae0dc44cb966bc736171a938c57d1fb582
Illarionov Anton <illarionovanton@gmail.com>
2022-03-27 16:43:02 +0300

    fix: fix issues with file names

--- log-parser/walk.c
+++ log-parser/walk.c
@@ -1,9 +1,10 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <dirent.h>
-#include <string.h>
-
 #include "walk.h"
+
+#include <dirent.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
 #include "utils.h"
 
 #define ACCESS_LOG "access.log"
@@ -19,11 +20,8 @@ int get_log_stats_from_dir(char* log_dir, LogFileStats** stats,
             if (!strncmp(ent->d_name, ACCESS_LOG, strlen(ACCESS_LOG))) {
                 stats[number_of_files] = malloc(sizeof(LogFileStats));
                 stats[number_of_files]->filename = calloc(sizeof(char), 64);
-                strncpy(stats[number_of_files]->filename, log_dir,
-                        strlen(log_dir));
-                stats[number_of_files]->filename[strlen(log_dir)] = '/';
-                strncpy(stats[number_of_files]->filename + strlen(log_dir) + 1,
-                        ent->d_name, MESSAGE_SIZE - strlen(log_dir));
+                sprintf(stats[number_of_files]->filename, "%s/%s", log_dir,
+                        ent->d_name);
                 stats[number_of_files]->requests_by_referrer =
                     create_hash_table();
                 stats[number_of_files]->traffic_by_url = create_hash_table();
